ad7143d99c759cd21f06e24c124e43e8f9b511d8
Mateusz Rytwiński <mateuszrytwinski@gmail.com>
2023-05-29 22:35:21 +0200

    Added CommandLineParser

---
+++ Models/ProgramOptions.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+
+namespace DifferentialEvolution.Models
+{
+    internal class ProgramOptions
+    {
+        [Option('n', "populationsize", Required = true, HelpText = "Population size [>=4]")]
+        public int PopulationSize { get; set; }
+
+        [Option('c', "crossoverprobability", Required = true, HelpText = "Crossover probability [0,1]")]
+        public double CrossoverProbability { get; set; }
+
+        [Option('f', "differentialweight", Required = true, HelpText = "Differential weight [0,2]")]
+        public double DifferentialWeight { get; set; }
+
+        [Option('i', "iterations", Required = true, HelpText = "Iterations [>=1]")]
+        public int Iterations { get; set; }
+
+        [Option('d', "dimensions", Required = true, HelpText = "Dimensions [>=1]")]
+        public int Dimensions { get; set; }
+
+        [Option('o', "function", Required = true, HelpText = "Function:\nSP - Sphere \nRA - Rastrigin \nMI - Michalewicz \nSC - Schwefel \nAC - Ackley \nGR - Griewank")]
+        public string Function { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using DifferentialEvolution.Models;
+﻿using CommandLine;
+
+using DifferentialEvolution.Models;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,120 +14,83 @@ namespace DifferentialEvolution
         {
             // https://en.wikipedia.org/wiki/Differential_evolution
 
-            var errors = new List<string>();
+            List<string> errors = new();
 
-            if(args.Length != 6)
+            Parameters parameters = Parser.Default.ParseArguments<ProgramOptions>(args).MapResult(x =>
             {
-                Console.WriteLine("Required arguments:");
-                Console.WriteLine("NP - population size [>=4]");
-                Console.WriteLine("CR - crossover probability [0,1]");
-                Console.WriteLine("F - differential weight [0,2]");
-                Console.WriteLine("I - iterations [>=1]");
-                Console.WriteLine("D - dimensions [>=1]");
-                Console.WriteLine("FU - function:");
-                Console.WriteLine("  SP - Sphere");
-                Console.WriteLine("  RA - Rastrigin");
-                Console.WriteLine("  MI - Michalewicz");
-                Console.WriteLine("  SC - Schwefel");
-                Console.WriteLine("  AC - Ackley");
-                Console.WriteLine("  GR - Griewank");
+                if (x.PopulationSize < 4)
+                    errors.Add("'-n (Population size)' has to be greater than or equal to 4");
+
+                if (x.CrossoverProbability < 0 || x.CrossoverProbability > 1)
+                    errors.Add("'-c (Crossover probability)' has to be between 0 and 1");
+
+                if (x.DifferentialWeight < 0 || x.DifferentialWeight > 2)
+                    errors.Add("'-f (Differential weight)' has to be between 0 and 2");
+
+                if (x.Iterations < 1)
+                    errors.Add("'-i (Iterations)' has to be greater than or equal to 1");
+
+                if (x.Dimensions < 1)
+                    errors.Add("'-d (Dimensions)' has to be greater than or equal to 1");
+
+                FitnessFunction fitnessFunction = null;
+                Tuple<double, double> domain = null;
+
+                switch (x.Function.ToUpper())
+                {
+                    case "SP":
+                        fitnessFunction = Functions.SphereFunction;
+                        domain = Functions.SphereDomain;
+                        break;
+
+                    case "RA":
+                        fitnessFunction = Functions.RastriginFunction;
+                        domain = Functions.RastriginDomain;
+                        break;
+
+                    case "MI":
+                        fitnessFunction = Functions.MichalewiczFunction;
+                        domain = Functions.MichalewiczDomain;
+                        break;
+
+                    case "SC":
+                        fitnessFunction = Functions.SchwefelFunction;
+                        domain = Functions.SchwefelDomain;
+                        break;
+
+                    case "AC":
+                        fitnessFunction = Functions.AckleyFunction;
+                        domain = Functions.AckleyDomain;
+                        break;
+
+                    case "GR":
+                        fitnessFunction = Functions.GriewankFunction;
+                        domain = Functions.GriewankDomain;
+                        break;
+
+                    default:
+                        errors.Add("'-o (Function)' option unrecognized");
+                        break;
+                }
+
+                return new Parameters()
+                {
+                    AgentsCount = x.PopulationSize,
+                    CR = x.CrossoverProbability,
+                    F = x.DifferentialWeight,
+                    Iterations = x.Iterations,
+                    Dimensions = x.Dimensions,
+                    FitnessFunction = fitnessFunction,
+                    Domain = domain,
+                };
+            }, x => null);
+
+            if (parameters == null)
                 return;
-            }
-            
-            // UWAGA - zakładamy określoną kolejność argumentów
-
-            if (!int.TryParse(args[0], out int np))
-            {
-                errors.Add("Cannot parse 'NP' to integer");
-            }
-
-            if (np < 4)
-            {
-                errors.Add("'NP' has to be greater than or equal to 4");
-            }
-
-            if (!double.TryParse(args[1], out double cr))
-            {
-                errors.Add("Cannot parse 'CR' to double");
-            }
-
-            if (cr < 0 || cr > 1)
-            {
-                errors.Add("'CR' has to be between 0 and 1");
-            }
-
-            if (!double.TryParse(args[2], out double f))
-            {
-                errors.Add("Cannot parse 'F' to double");
-            }
-
-            if (f < 0 || f > 2)
-            {
-                errors.Add("'F' has to be between 0 and 2");
-            }
-
-            if (!int.TryParse(args[3], out int i))
-            {
-                errors.Add("Cannot parse 'I' to integer");
-            }
-
-            if (i < 1)
-            {
-                errors.Add("'I' has to be greater than or equal to 1");
-            }
-
-            if (!int.TryParse(args[4], out int d))
-            {
-                errors.Add("Cannot parse 'D' to integer");
-            }
-
-            if (d < 1)
-            {
-                errors.Add("'D' has to be greater than or equal to 1");
-            }
-
-            FitnessFunction fitnessFunction = null;
-            Tuple<double, double> domain = null;
-
-            switch (args[5].ToUpper())
-            {
-                case "SP":
-                    fitnessFunction = Functions.SphereFunction;
-                    domain = Functions.SphereDomain;
-                    break;
-
-                case "RA":
-                    fitnessFunction = Functions.RastriginFunction;
-                    domain = Functions.RastriginDomain;
-                    break;
-
-                case "MI":
-                    fitnessFunction = Functions.MichalewiczFunction;
-                    domain = Functions.MichalewiczDomain;
-                    break;
-
-                case "SC":
-                    fitnessFunction = Functions.SchwefelFunction;
-                    domain = Functions.SchwefelDomain;
-                    break;
-
-                case "AC":
-                    fitnessFunction = Functions.AckleyFunction;
-                    domain = Functions.AckleyDomain;
-                    break;
-
-                case "GR":
-                    fitnessFunction = Functions.GriewankFunction;
-                    domain = Functions.GriewankDomain;
-                    break;
-
-                default:
-                    errors.Add("'FU' value unrecognized");
-                    break;
-            }
 
             if (errors.Any())
             {
+                Console.WriteLine("Errors:");
                 Console.WriteLine(string.Join("\n", errors));
                 return;
             }
@@ -133,18 +99,7 @@ namespace DifferentialEvolution
             // 1. Umożliwić podawanie argumentów, także funkcji testowej [done]
             // 2. Pokazać postęp przy każdej iteracji (jakiś output częściowy, nie tylko finalny) [done]
 
-            var parameters = new Parameters
-            {
-                AgentsCount = np,
-                CR = cr,
-                F = f,
-                Iterations = i,
-                Dimensions = d,
-                FitnessFunction = fitnessFunction,
-                Domain = domain,
-            };
-
-            var solver = new Solver(parameters);
+            Solver solver = new(parameters);
             solver.Run();
         }
     }
